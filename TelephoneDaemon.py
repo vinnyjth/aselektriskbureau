5990ab5af25ac8ce75088a7d19092a2827bc8a79
Vincent Wilson <vincent.movieman@gmail.com>
2016-06-12 21:24:47 -0400

    Added soundbyte support

--- TelephoneDaemon.py
+++ TelephoneDaemon.py
@@ -1,19 +1,11 @@
-# import os
-# import Queue
 import threading
-# import signal
 import sys
-# import yaml
+import pygame.mixer
+import os.path
 
 from threading import Timer
-# from modules.Ringtone import Ringtone
 from modules.RotaryDial import RotaryDial
-# from modules.Webserver import Webserver
-# from modules.linphone import Wrapper
-# alternative SIP-implementation
-#from modules.pjsip.SipClient import SipClient
-
-# callback_queue = Queue.Queue()
+from pygame.mixer import Sound
 
 class TelephoneDaemon:
     # Number to be dialed
@@ -26,51 +18,30 @@ class TelephoneDaemon:
     offHookTimeoutTimer = None
 
     RotaryDial = None
-    # SipClient = None
-    # WebServer = None
 
     # config = None
 
     def __init__(self):
         print "[STARTUP]"
 
-        # self.config = yaml.load(file("configuration.yml",'r'))
-
-        # signal.signal(signal.SIGINT, self.OnSignal)
-
-        # Ring tone
-        # self.Ringtone = Ringtone(self.config)
-
-        # This is to indicate boot complete. Not very realistic, but fun.
-        #self.Ringtone.playfile(config["soundfiles"]["startup"])
-
         # Rotary dial
         self.RotaryDial = RotaryDial()
         self.RotaryDial.RegisterCallback(NumberCallback = self.GotDigit, OffHookCallback = self.OffHook, OnHookCallback = self.OnHook, OnVerifyHook = self.OnVerifyHook)
-
-        # self.SipClient = Wrapper.Wrapper()
-        # self.SipClient.StartLinphone()
-        # self.SipClient.SipRegister(self.config["sip"]["username"], self.config["sip"]["hostname"], self.config["sip"]["password"])
-        # self.SipClient.RegisterCallbacks(OnIncomingCall = self.OnIncomingCall, OnOutgoingCall = self.OnOutgoingCall, OnRemoteHungupCall = self.OnRemoteHungupCall, OnSelfHungupCall = self.OnSelfHungupCall)
-
-        # Start SipClient thread
-        # self.SipClient.start()
-
-        # Web interface to enable remote configuration and debugging.
-        # self.Webserver = Webserver(self)
+        pygame.mixer.init()
 
         raw_input("Waiting.\n")
 
     def OnHook(self):
         print "[PHONE] On hook"
         self.offHook = False
-        # self.Ringtone.stophandset()
-        # # Hang up calls
-        # if self.SipClient is not None:
-        #     self.SipClient.SipHangup()
 
     def OffHook(self):
         print "[PHONE] Off hook"
+        fileName = "sound/%s.wav" % self.dial_number
+        if os.path.isfile(fname):
+            Sound().play()
+        else:
+            Sound("sound/default.wav").play()
         self.offHook = True
         # Reset current number when off hook
         self.dial_number = ""
@@ -79,38 +50,9 @@ class TelephoneDaemon:
         self.offHookTimeoutTimer = Timer(5, self.OnOffHookTimeout)
         self.offHookTimeoutTimer.start()
 
-        # TODO: State for ringing, don't play tone if ringing :P
-        # print "Try to start dialtone"
-        # self.Ringtone.starthandset(self.config["soundfiles"]["dialtone"])
-        #
-        # self.Ringtone.stop()
-        # if self.SipClient is not None:
-        #     self.SipClient.SipAnswer()
-
     def OnVerifyHook(self, state):
         if not state:
             self.offHook = False
-            # self.Ringtone.stophandset()
-
-    def OnIncomingCall(self):
-        print "[INCOMING]"
-        # self.Ringtone.start()
-
-    def OnOutgoingCall(self):
-        print "[OUTGOING] "
-
-    def OnRemoteHungupCall(self):
-        print "[HUNGUP] Remote disconnected the call"
-        # Now we want to play busy-tone..
-        # self.Ringtone.starthandset(self.config["soundfiles"]["busytone"])
-
-    def OnSelfHungupCall(self):
-        print "[HUNGUP] Local disconnected the call"
-
-    def OnOffHookTimeout(self):
-        print "[OFFHOOK TIMEOUT]"
-        #self.Ringtone.stophandset()
-        #self.Ringtone.starthandset(self.config["soundfiles"]["timeout"])
 
     def GotDigit(self, digit):
         print "[DIGIT] Got digit: %s" % digit
@@ -133,8 +75,6 @@ class TelephoneDaemon:
 
     def OnSignal(self, signal, frame):
         print "[SIGNAL] Shutting down on %s" % signal
-        # self.RotaryDial.StopVerifyHook()
-        # self.SipClient.StopLinphone()
         sys.exit(0)
 
 def main():
